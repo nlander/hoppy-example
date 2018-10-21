3daf63258c39bc55b4c86f6f3b3df772046bcf9f
Nathan Lander <lander89@gmail.com>
2018-10-21 18:32:52 -0500

    run stack build

---
+++ example-cpp/cpp/gen_utils.hpp
@@ -0,0 +1,10 @@
+////////// GENERATED FILE, EDITS WILL BE LOST //////////
+
+#ifndef HOPPY_MODULE_example_utils
+#define HOPPY_MODULE_example_utils
+
+extern "C" {
+
+}  // extern "C"
+
+#endif  // ifndef HOPPY_MODULE_example_utils
